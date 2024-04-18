e90a3180366ed6311472160c9bdcec086f6d074d
Manh Tu VU <glmanhtu@gmail.com>
2024-04-18 11:40:05 +0200

    Update

--- src/canvas-force-graph.js
+++ src/canvas-force-graph.js
@@ -82,7 +82,6 @@ export default Kapsule({
     linkDirectionalParticleColor: { triggerUpdate: false },
     globalScale: { default: 1, triggerUpdate: false },
     d3AlphaMin: { default: 0, triggerUpdate: false},
-    d3Alpha: { default: 1, triggerUpdate: false},
     d3AlphaDecay: { default: 0.0228, triggerUpdate: false, onChange(alphaDecay, state) { state.forceLayout.alphaDecay(alphaDecay) }},
     d3AlphaTarget: { default: 0, triggerUpdate: false, onChange(alphaTarget, state) { state.forceLayout.alphaTarget(alphaTarget) }},
     d3VelocityDecay: { default: 0.4, triggerUpdate: false, onChange(velocityDecay, state) { state.forceLayout.velocityDecay(velocityDecay) } },
